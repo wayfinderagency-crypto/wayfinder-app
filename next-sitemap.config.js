1d258113e73c392a544cd0e5bb372d80fc6b89ae
Gracjan <Gracjanwi@interia.pl>
2025-11-12 21:43:30 +0100

    change sitemap to the last one

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "wayfinder-agency.com", // <- zamień na swój adres strony
+  siteUrl: "http://wayfinder-agency.com", // <- zamień na swój adres strony
   generateRobotsTxt: true, // tworzy automatycznie robots.txt
+  outDir: "./public",
   changefreq: "weekly", // jak często Google powinien sprawdzać strony
   priority: 0.8, // domyślny priorytet stron
   sitemapSize: 5000, // maksymalna liczba URL w jednym pliku sitemap
