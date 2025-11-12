18367180208eba818ecb18c170b8035a1df2e05e
Gracjan <Gracjanwi@interia.pl>
2025-11-12 21:33:39 +0100

    change sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://wayfinder-agency.com", // <- zamień na swój adres strony
+  siteUrl: "wayfinder-agency.com", // <- zamień na swój adres strony
   generateRobotsTxt: true, // tworzy automatycznie robots.txt
   changefreq: "weekly", // jak często Google powinien sprawdzać strony
   priority: 0.8, // domyślny priorytet stron
